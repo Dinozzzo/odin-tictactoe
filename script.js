72ff899575d27e35b4acd54574c08b8cba23cb1c
Dinozzzo <dleootta@gmail.com>
2025-10-05 14:31:10 +0200

    Design done, structure of the code created

---
+++ script.js
@@ -0,0 +1,15 @@
+function tictactoe() {
+  const gameboard = {
+    table: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  };
+
+  function player(name, symbol, score) {
+    return {
+      name,
+      symbol,
+      score,
+    };
+  }
+
+  const gameController = {};
+}

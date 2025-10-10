510bb854216a7e928fddc8450c782aada0a945eb
Dinozzzo <dleootta@gmail.com>
2025-10-10 15:40:16 +0200

    Inputs created to choose place and symbol. NEXT STEP : Add winning condition

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function tictactoe() {
   const gameboard = (() => {
-    // MY ARRAY REPRESENTING THE GAME DISPLAY
+    // ARRAY REPRESENTING THE GAME DISPLAY
     let table = ["", "", "", "", "", "", "", "", ""];
 
     // PLACE THE SYMBOL IN A SPECIFIC POSITION
     const addSymbol = (pos, symbol) => {
       table[pos] = symbol;
-      console.log(table);
+      console.table(table);
     };
     return { table, addSymbol };
   })();
@@ -18,18 +18,36 @@ function tictactoe() {
       symbol: symbol,
       score: score,
     };
-  } // ADD THIS INSIDE GAMECONTROLLER ??
+  }
 
   const gameController = (() => {
     // CREATING PLAYERS 1 & 2
-    let player1 = createPlayer("Dino", "X", 0);
-    let player2 = createPlayer("Diah", "O", 0);
+    let player1 = createPlayer("Player 1", "X", 0);
+    let player2 = createPlayer("Player 2", "O", 0);
 
     // HOW THE GAME WILL BE PLAYED
-    function playGame() {}
+    const playGame = () => {
+      const playerOneChoice = prompt(
+        "Player 1 : Choose a position between 0 and 8"
+      );
+      const playerTwoChoice = prompt(
+        "Player 2 : Choose a position between 0 and 8"
+      );
+
+      gameboard.addSymbol(playerOneChoice, player1.symbol);
+      gameboard.addSymbol(playerTwoChoice, player2.symbol);
+    };
 
     function finishRound() {}
 
     function endGame() {}
+
+    return {
+      playGame,
+    };
   })();
+
+  gameController.playGame();
 }
+
+tictactoe();

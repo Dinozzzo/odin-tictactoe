b98ff1954783c6bdcef87c3a4f4ff86744a300e9
Dinozzzo <dleootta@gmail.com>
2025-10-13 23:49:39 +0200

    Now I need to create a loop to play again if no win or tie

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function tictactoe() {
   const gameboard = (() => {
     // ARRAY REPRESENTING THE GAME DISPLAY
-    let table = ["", "", "", "", "", "", "", "", ""];
+    let table = [null, null, null, null, null, null, null, null, null];
 
     // PLACE THE SYMBOL IN A SPECIFIC POSITION
     const addSymbol = (pos, symbol) => {
@@ -27,20 +27,52 @@ function tictactoe() {
 
     // HOW THE GAME WILL BE PLAYED
     const playGame = () => {
+      // DIFFERENT WINNING COMBINATIONS
+      let winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      // PLAYER 1 ROUND
       const playerOneChoice = prompt(
         "Player 1 : Choose a position between 0 and 8"
       );
+      gameboard.addSymbol(playerOneChoice, player1.symbol);
+
+      // CHECKS IF PLAYER 1 WON
+      for (let value of winningCombinations) {
+        if (
+          gameboard.table[value[0]] === "X" &&
+          gameboard.table[value[1]] === "X" &&
+          gameboard.table[value[2]] === "X"
+        ) {
+          console.log("PLAYER 1 WIN");
+        }
+      }
+
+      // PLAYER 2 ROUND
       const playerTwoChoice = prompt(
         "Player 2 : Choose a position between 0 and 8"
       );
-
-      gameboard.addSymbol(playerOneChoice, player1.symbol);
       gameboard.addSymbol(playerTwoChoice, player2.symbol);
+
+      // CHECKS IF PLAYER 2 WON
+      for (let value of winningCombinations) {
+        if (
+          gameboard.table[value[0]] === "O" &&
+          gameboard.table[value[1]] === "O" &&
+          gameboard.table[value[2]] === "O"
+        ) {
+          console.log("PLAYER 2 WIN");
+        }
+      }
     };
-
-    function finishRound() {}
-
-    function endGame() {}
 
     return {
       playGame,
